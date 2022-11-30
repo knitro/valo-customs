aefbbab5490c6ec6a1b47c82fbfabd87f434c0ac
Calvin Lee <calvin.hanbee.lee@gmail.com>
2022-11-30 21:16:08 +1300

    Create base home page

---
+++ src/components/Home/HomeCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-hover v-slot="{ hover }">
+    <v-card
+      class="mx-auto"
+      color="grey lighten-4"
+      max-width="600"
+      @click="linkToPage(link)"
+    >
+      <v-img :aspect-ratio="16 / 14" :src="image"></v-img>
+      <v-card-text class="pt-6" style="position: relative">
+        <div class="font-weight-light grey--text text-h6 mb-2">
+          {{ subheader }}
+        </div>
+        <h3 class="text-h4 font-weight-light purple--text mb-2">
+          {{ header }}
+        </h3>
+      </v-card-text>
+      <v-expand-transition>
+        <div
+          v-if="hover"
+          class="d-flex transition-fast-in-fast-out purple darken-2 v-card--reveal text-h2 white--text"
+          style="height: 100%"
+        ></div>
+      </v-expand-transition>
+    </v-card>
+  </v-hover>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import router from "@/router";
+
+export default Vue.extend({
+  name: "HomeCard",
+
+  components: {},
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+    header: {
+      type: String,
+      required: true,
+    },
+    subheader: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    linkToPage(href: string) {
+      router.push(href);
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.5;
+  position: absolute;
+  width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,58 @@
 <template>
-  <div></div>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col
+          v-for="(cardData, index) in homeCardData"
+          v-bind:key="index + '-' + cardData.header"
+          cols="4"
+        >
+          <HomeCard
+            :image="cardData.image"
+            :header="cardData.header"
+            :subheader="cardData.subheader"
+            :link="cardData.link"
+          ></HomeCard>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import HomeCard from "@/components/Home/HomeCard.vue";
+import { HomeCardItem } from "@/components/Home/HomeCardInterfaces";
 
 export default Vue.extend({
   name: "HomeView",
-
-  components: {},
+  components: {
+    HomeCard,
+  },
+  data() {
+    const homeCardData: HomeCardItem[] = [
+      {
+        image: "https://cdn.vuetifyjs.com/images/cards/kitchen.png",
+        header: "Random Map",
+        subheader: "Quickly pick a random map",
+        link: "/random",
+      },
+      {
+        image: "https://cdn.vuetifyjs.com/images/cards/kitchen.png",
+        header: "Local PickBan",
+        subheader: "Find the perfect map for your customs",
+        link: "/bestOfOne",
+      },
+      {
+        image: "https://cdn.vuetifyjs.com/images/cards/kitchen.png",
+        header: "Online",
+        subheader: "Use the Pick Ban System with people online",
+        link: "/online",
+      },
+    ];
+    return {
+      homeCardData: homeCardData,
+    };
+  },
 });
 </script>
