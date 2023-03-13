9a3e13c53ec293ffdc85fcb7df2cb5a8f1ebf3f3
Calvin Lee <calvin.hanbee.lee@gmail.com>
2023-03-14 01:16:36 +1300

    Add images and card transparency

--- src/components/Card/TeamCard/TeamCard.vue
+++ src/components/Card/TeamCard/TeamCard.vue
@@ -1,20 +1,21 @@
 <template>
-  <v-card>
-    <v-img
-      src="https://picsum.photos/1920/1080?random"
-      :gradient="'to top right, ' + color + ', rgba(25,32,72,.7)'"
-      height="112"
-      class="bottom-border-radius"
-    >
-      <v-card-text>
-        <div>{{ subtitle }}</div>
-        <div class="text-h4 text--primary">
-          {{ title }}
-        </div>
-      </v-card-text>
-    </v-img>
-
-    <v-list>
+  <v-card :color="backgroundColour">
+    <div class="background-color-white-transparent">
+      <v-img
+        :src="headerImage"
+        :gradient="'to top right, ' + color + ', rgba(25,32,72,.7)'"
+        height="112"
+        class="bottom-border-radius"
+      >
+        <v-card-text>
+          <div>{{ subtitle }}</div>
+          <div class="text-h4 text--primary">
+            {{ title }}
+          </div>
+        </v-card-text>
+      </v-img>
+    </div>
+    <v-list :color="backgroundColour">
       <v-list-item v-show="players.length === 0">
         <i>No players</i>
       </v-list-item>
@@ -53,12 +54,28 @@ export default Vue.extend({
       type: String,
       required: true,
     },
+    headerImage: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      backgroundColour: "rgba(255, 255, 255, 0.5)",
+    };
   },
 });
 </script>
 
 <style scoped lang="scss">
 .bottom-border-radius {
-  border-radius: 10px;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  border-end-start-radius: 12px;
+  border-end-end-radius: 12px;
+}
+
+.background-color-white-transparent {
+  background-color: rgba(255, 255, 255, 0.5);
 }
 </style>
--- src/views/TeamSelectView.vue
+++ src/views/TeamSelectView.vue
@@ -1,121 +1,145 @@
 <template>
   <div>
-    <app-bar-valo-custom title="Team Select"></app-bar-valo-custom>
+    <v-img
+      :src="backgroundImage"
+      :min-height="windowHeight"
+      :max-height="windowHeight"
+    >
+      <app-bar-valo-custom title="Team Select"></app-bar-valo-custom>
 
-    <v-row v-show="state === 0" no-gutters>
-      <v-col>
-        <v-card class="grid-margin">
-          <v-card-title>Player List</v-card-title>
-          <v-card-subtitle>
-            Player Count:
-            <b>{{ playerNames.length }}</b>
-          </v-card-subtitle>
-          <v-list>
-            <v-list-item v-show="playerNames.length === 0">
-              <i>
-                No players added yet! To add a player, enter a player name in
-                the text field on the right.
-              </i>
-            </v-list-item>
-            <v-list-item v-for="(item, index) in playerNames" :key="index">
-              <v-list-item-avatar>
-                <v-icon class="purple lighten-1" dark>mdi-account</v-icon>
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title>{{ item }}</v-list-item-title>
-              </v-list-item-content>
-              <v-list-item-action>
-                <v-btn icon @click="removePlayer(index)">
-                  <v-icon color="red">mdi-close</v-icon>
-                </v-btn>
-              </v-list-item-action>
-            </v-list-item>
-          </v-list>
-        </v-card>
-      </v-col>
+      <v-row v-show="state === 0" no-gutters>
+        <v-col cols="6">
+          <v-card class="grid-margin" :color="cardBackgroundColour">
+            <div class="background-color-white-transparent">
+              <v-img
+                src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg"
+                :gradient="'to top right, rgba(103,58,183,1), rgba(25,32,72,.7)'"
+                height="112"
+                class="bottom-border-radius"
+              >
+                <v-card-title>Player List</v-card-title>
+                <v-card-subtitle>
+                  Player Count:
+                  <b>{{ playerNames.length }}</b>
+                </v-card-subtitle>
+              </v-img>
+            </div>
+            <v-list :color="cardBackgroundColour">
+              <v-list-item v-show="playerNames.length === 0">
+                <i>
+                  No players added yet! To add a player, enter a player name in
+                  the text field on the right.
+                </i>
+              </v-list-item>
+              <v-list-item v-for="(item, index) in playerNames" :key="index">
+                <v-list-item-avatar>
+                  <v-icon class="purple lighten-1" dark>mdi-account</v-icon>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title>{{ item }}</v-list-item-title>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-btn icon @click="removePlayer(index)">
+                    <v-icon color="red">mdi-close</v-icon>
+                  </v-btn>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-col>
 
-      <v-col>
-        <v-text-field
-          append-outer-icon="mdi-send"
-          v-model="inputValue"
-          outlined
-          height="50"
-          class="team-select-text-input grid-margin"
-          label="Player Name"
-          color="purple"
-          @keydown.enter="addNewPlayer"
-          @click:append-outer="addNewPlayer"
-          clear-icon="mdi-close-circle"
-          clearable
-        >
-        </v-text-field>
-        <v-row class="grid-margin">
-          <v-col>
-            <v-btn x-large block @click="clearPlayerList" color="warning">
-              <v-icon left dark> mdi-trash-can-outline </v-icon>
-              Clear Player List
-            </v-btn>
-          </v-col>
-          <v-col>
-            <v-btn x-large block @click="createTeams" color="deep-purple" dark>
-              <v-icon left dark> mdi-account-group </v-icon>
-              Create Teams
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
+        <v-col cols="6">
+          <v-text-field
+            append-outer-icon="mdi-send"
+            v-model="inputValue"
+            outlined
+            height="50"
+            class="team-select-text-input grid-margin"
+            label="Player Name"
+            color="purple"
+            @keydown.enter="addNewPlayer"
+            @click:append-outer="addNewPlayer"
+            clear-icon="mdi-close-circle"
+            clearable
+          >
+          </v-text-field>
+          <v-row class="grid-margin">
+            <v-col>
+              <v-btn x-large block @click="clearPlayerList" color="warning">
+                <v-icon left dark> mdi-trash-can-outline </v-icon>
+                Clear Player List
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn
+                x-large
+                block
+                @click="createTeams"
+                color="deep-purple"
+                dark
+              >
+                <v-icon left dark> mdi-account-group </v-icon>
+                Create Teams
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
 
-    <v-row v-show="state === 1" no-gutters>
-      <v-col cols="4">
-        <team-card
-          title="Team 1"
-          subtitle="Attacker Side"
-          color="#EF5351"
-          :players="teamOne"
-          class="grid-margin"
-        ></team-card>
-      </v-col>
-      <v-col cols="4">
-        <team-card
-          title="Team 2"
-          subtitle="Defender Side"
-          color="#16E5B4"
-          :players="teamTwo"
-          class="grid-margin"
-        ></team-card>
-      </v-col>
-      <v-col cols="4">
-        <team-card
-          title="Spectators"
-          subtitle="Observers"
-          color="grey"
-          :players="spectators"
-          class="grid-margin"
-        ></team-card>
-        <v-row>
-          <v-col class="grid-margin">
-            <v-btn x-large block @click="createTeams" color="warning">
-              <v-icon left dark> mdi-redo </v-icon>
-              Recreate Teams
-            </v-btn>
-          </v-col>
-          <v-col class="grid-margin">
-            <v-btn
-              x-large
-              block
-              @click="addPlayersView"
-              color="deep-purple"
-              dark
-            >
-              <v-icon left dark> mdi-arrow-left </v-icon>
-              Change Player List
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-row v-show="state === 1"> </v-row>
+      <v-row v-show="state === 1" no-gutters>
+        <v-col cols="4">
+          <team-card
+            title="Team 1"
+            subtitle="Attacker Side"
+            color="#EF5351"
+            :players="teamOne"
+            class="grid-margin"
+            :headerImage="backgroundTeamOne"
+          ></team-card>
+        </v-col>
+        <v-col cols="4">
+          <team-card
+            title="Team 2"
+            subtitle="Defender Side"
+            color="#16E5B4"
+            :players="teamTwo"
+            class="grid-margin"
+            :headerImage="backgroundTeamTwo"
+          ></team-card>
+        </v-col>
+        <v-col cols="4">
+          <team-card
+            title="Spectators"
+            subtitle="Observers"
+            color="grey"
+            :players="spectators"
+            class="grid-margin"
+            :headerImage="backgroundSpectator"
+          ></team-card>
+          <v-row>
+            <v-col class="grid-margin">
+              <v-btn x-large block @click="createTeams" color="warning">
+                <v-icon left dark> mdi-redo </v-icon>
+                Recreate Teams
+              </v-btn>
+            </v-col>
+            <v-col class="grid-margin">
+              <v-btn
+                x-large
+                block
+                @click="addPlayersView"
+                color="deep-purple"
+                dark
+              >
+                <v-icon left dark> mdi-arrow-left </v-icon>
+                Change Player List
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row v-show="state === 1"> </v-row>
+    </v-img>
   </div>
 </template>
 
@@ -141,6 +165,14 @@ export default Vue.extend({
       teamOne: [] as string[],
       teamTwo: [] as string[],
       spectators: [] as string[],
+
+      backgroundTeamOne: require("@/assets/images/range-attack.png"),
+      backgroundTeamTwo: require("@/assets/images/range-defense.png"),
+      backgroundSpectator: require("@/assets/images/outside.png"),
+      backgroundImage: require("@/assets/backgrounds/range-bots.png"),
+
+      windowHeight: window.innerHeight,
+      cardBackgroundColour: "rgba(255, 255, 255, 0.5)",
     };
   },
   methods: {
@@ -207,5 +239,16 @@ export default Vue.extend({
 
 .grid-margin {
   margin: 10px;
+}
+
+.bottom-border-radius {
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  border-end-start-radius: 12px;
+  border-end-end-radius: 12px;
+}
+
+.background-color-white-transparent {
+  background-color: rgba(255, 255, 255, 0.5);
 }
 </style>
